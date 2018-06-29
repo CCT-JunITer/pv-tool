b5fde7c365899cfe9581889cfba1b0a9fc8a2a08
vmk1vmk <vitalij@vitalij-laptop.fritz.box>
2018-06-29 11:25:04 +0200

    Cleaned up toggle handlers for filter and user mask drawer.

--- src/containers/Topbar/Topbar.js
+++ src/containers/Topbar/Topbar.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import * as actions from '../../store/actions/actions'
 import Dropdownbar from '../../components/DropdownBar/DropdownBar';
 import Aux from '../../hoc/AuxComponent/AuxComponent'
@@ -8,54 +8,50 @@ import FilterDrawer from '../../components/FilterDrawer/FilterDrawer'
 import UserMaskDrawer from '../../components/UserMaskDrawer/UserMaskDrawer'
 
 class Topbar extends Component {
-    state = {
+  state = {
+    expandFilter: false,
+    expandUserMask: false
+  };
+
+  resetDrawers () {
+    this.setState({
       expandFilter: false,
       expandUserMask: false
-    };
+    })
+  }
 
-    filterToggelHandler = () => {
-      if (!this.state.expandFilter) {
-        this.setState ({ expandFilter: true, expandUserMask: false })
-      } else {
-        this.setState ({ expandFilter: false })
-      }
-      
-      console.log("FILTER TOGGLE")
-    };
- 
-    userMaskToggleHandler = () => {
-      if (!this.state.expandUserMask) {
-        this.setState ({ expandFilter: false, expandUserMask: true })
-      } else {
-        this.setState ({ expandUserMask: false })
-      }
+  filterToggelHandler () {
+    this.resetDrawers();
+    if (!this.state.expandFilter) this.setState({ expandFilter: true });
+  }
 
-      console.log("USER MASK TOGGLE")
-    };
+  userMaskToggleHandler () {
+    this.resetDrawers();
+    if (!this.state.expandUserMask) this.setState({ expandFilter: false });
+  }
 
-
-    render(){
-        return(
-            <Aux>
-              <Toolbar
-                openFilter={this.state.expandFilter}
-                filterToggle={this.filterToggelHandler.bind(this)}
-                userMaskToggle={this.userMaskToggleHandler.bind(this)}
-                style={{zIndex: '100000'}} />
-              <Dropdownbar
-                change={this.props.dropDownChanged}
-                style={{zIndex: '100000'}} />
-              <FilterDrawer open={this.state.expandFilter} />
-              <UserMaskDrawer open={this.state.expandUserMask} />
-            </Aux>
-        );
-    }
+  render(){
+    return(
+      <Aux>
+        <Toolbar
+          openFilter={this.state.expandFilter}
+          filterToggle={this.filterToggelHandler.bind(this)}
+          userMaskToggle={this.userMaskToggleHandler.bind(this)}
+          style={{zIndex: '100000'}} />
+        <Dropdownbar
+          change={this.props.dropDownChanged}
+          style={{zIndex: '100000'}} />
+        <FilterDrawer open={this.state.expandFilter} />
+        <UserMaskDrawer open={this.state.expandUserMask} />
+      </Aux>
+    );
+  }
 }
 
 const mapDispatchToProps  = dispatch => {
-    return {
-        dropDownChanged:(col, element) => dispatch(actions.dropdownChanged(col, element))
-    }
+  return {
+    dropDownChanged:(col, element) => dispatch(actions.dropdownChanged(col, element))
+  }
 };
 
 export default connect(null, mapDispatchToProps)(Topbar);
